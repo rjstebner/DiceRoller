7a130bccaed26941c8e1110f7302569a3c76fe66
rjstebner <rjstebner@gmail.com>
2025-08-09 08:31:20 +0900

    changed ddice image

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function rollDice() {
   for (let i = 0; i < diceCount; i++) {
     const roll = Math.floor(Math.random() * 6) + 1;
     const img = document.createElement("img");
-    img.src = `https://raw.githubusercontent.com/Ennoriel/Dice-Icons/main/dice${roll}.png`;
+    img.src = `images/dice${roll}.png`; // Local file instead of remote
     img.alt = `Dice showing ${roll}`;
     img.classList.add("dice");
     diceContainer.appendChild(img);
